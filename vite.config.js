2f3907041aabb2cc40e5a48f507435fa108c378e
egorsv1985 <egorvs2009@gmail.com>
2024-01-04 12:44:50 +0300

    add docs

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import Inspect from "vite-plugin-inspect";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
-export default defineConfig(() => {
-  return {
-    plugins: [
-      ViteImageOptimizer({
-        jpg: {
-          quality: 80,
-        },
-      }),
-    ],
-  };
+export default defineConfig({
+  build: {
+    outDir: "docs", // Измените на желаемое вами название папки
+  },
 });
